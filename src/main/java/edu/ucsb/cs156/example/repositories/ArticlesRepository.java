36446e5609b668ac44f0564bad30733fe1b7f48c
hyi96 <yihao0906@gmail.com>
2024-02-01 22:35:51 -0800

    hy creared database table for Articles

--- src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/ArticlesRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticlesRepository extends CrudRepository<Articles, Long> {
-  Iterable<Articles> findAllByQuarterYYYYQ(String quarterYYYYQ);
+
 }
